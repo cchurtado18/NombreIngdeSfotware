0ea58e2a1846b45bf9acd635a03e36ed40df3e6c
cchurtado18 <cchurtado@uamv.edu.ni>
2023-05-04 23:32:06 -0600

    Userlog

---
+++ MiBaseDatos/Userlog.sql
@@ -0,0 +1,59 @@
+INSERT INTO Userlog (idUsuario,name, rol, entityName,loginDateTime,LogOutDateTime,docId,docName,machineId,reportId)
+VALUES (1, 'carlos' , 3, 'uamv' , '11:54 am' , '12:31 pm' , 1 , 'pesca' , 12 , 2)
+
+INSERT INTO Userlog (idUsuario,name, rol, entityName,loginDateTime,LogOutDateTime,docId,docName,machineId,reportId) 
+VALUES (2, 'Ana', 1, 'Banco', '10:20 am', '11:45 am', 2, 'Factura', 10, 3)
+
+INSERT INTO Userlog (idUsuario,name, rol, entityName,loginDateTime,LogOutDateTime,docId,docName,machineId,reportId) 
+VALUES (3, 'Pedro', 2, 'Hospital', '2:00 pm', '4:15 pm', 3, 'Historia Clínica', 8, 4)
+
+INSERT INTO Userlog (idUsuario,name, rol, entityName,loginDateTime,LogOutDateTime,docId,docName,machineId,reportId) 
+VALUES (4, 'Marta', 3, 'Escuela', '8:30 am', '12:00 pm', 4, 'Examen Final', 15, 5)
+
+INSERT INTO Userlog (idUsuario,name, rol, entityName,loginDateTime,LogOutDateTime,docId,docName,machineId,reportId) 
+VALUES (5, 'Juan', 1, 'Tienda', '1:30 pm', '3:45 pm', 5, 'Recibo de compra', 7, 6)
+
+INSERT INTO Userlog (idUsuario,name, rol, entityName,loginDateTime,LogOutDateTime,docId,docName,machineId,reportId) 
+VALUES (6, 'Luisa', 2, 'Gimnasio', '6:00 am', '7:30 am', 6, 'Rutina de ejercicios', 21, 7)
+
+INSERT INTO Userlog (idUsuario,name, rol, entityName,loginDateTime,LogOutDateTime,docId,docName,machineId,reportId)
+VALUES (7, 'Raúl', 3, 'Librería', '9:15 am', '10:30 am', 7, 'Factura de compra', 11, 8)
+
+INSERT INTO Userlog (idUsuario,name, rol, entityName,loginDateTime,LogOutDateTime,docId,docName,machineId,reportId) 
+VALUES (8, 'Sara', 1, 'Agencia de viajes', '4:00 pm', '6:15 pm', 8, 'Boleto de avión', 19, 9)
+
+INSERT INTO Userlog (idUsuario,name, rol, entityName,loginDateTime,LogOutDateTime,docId,docName,machineId,reportId)
+VALUES (9, 'Mario', 2, 'Cafetería', '11:00 am', '12:30 pm', 9, 'Ticket de compra', 13, 10)
+
+INSERT INTO Userlog (idUsuario,name, rol, entityName,loginDateTime,LogOutDateTime,docId,docName,machineId,reportId)
+VALUES (10, 'Camila', 3, 'Farmacia', '7:45 am', '8:30 am', 10, 'Receta médica', 18, 11)
+
+INSERT INTO Userlog (idUsuario,name, rol, entityName,loginDateTime,LogOutDateTime,docId,docName,machineId,reportId) 
+VALUES (11, 'Paola', 1, 'Hotel', '3:30 pm', '5:00 pm', 11, 'Reservación', 22, 12)
+
+INSERT INTO Userlog (idUsuario,name, rol, entityName,loginDateTime,LogOutDateTime,docId,docName,machineId,reportId)
+VALUES (12, 'David', 2, 'Estación de gasolina', '9:00 am', '10:15 am', 12, 'Ticket de compra', 16, 13)
+
+INSERT INTO Userlog (idUsuario,name, rol, entityName,loginDateTime,LogOutDateTime,docId,docName,machineId,reportId) 
+VALUES (13, 'Laura', 3, 'Spa', '2:15 pm', '4:30 pm', 13, 'Reservación de masaje', 9, 14)
+
+INSERT INTO Userlog (idUsuario,name, rol, entityName,loginDateTime,LogOutDateTime,docId,docName,machineId,reportId) 
+VALUES (14, 'Diego', 1, 'Taller mecánico', '8:00 am', '11:30 am', 14, 'Factura de reparación', 14, 15)
+
+INSERT INTO Userlog (idUsuario,name, rol, entityName,loginDateTime,LogOutDateTime,docId,docName,machineId,reportId) 
+VALUES (15, 'María', 2, 'Consultorio dental', '10:45 am', '11:30 am', 15, 'Historia clínica dental', 23, 16)
+
+INSERT INTO Userlog (idUsuario,name, rol, entityName,loginDateTime,LogOutDateTime,docId,docName,machineId,reportId)
+VALUES (16, 'Gustavo', 3, 'Cine', '6:30 pm', '9:45 pm', 16, 'Boleto de entrada', 5, 17)
+
+INSERT INTO Userlog (idUsuario,name, rol, entityName,loginDateTime,LogOutDateTime,docId,docName,machineId,reportId)
+VALUES (17, 'Adriana', 1, 'Museo', '11:00 am', '2:15 pm', 17, 'Boleto de entrada', 17, 18)
+
+INSERT INTO Userlog (idUsuario,name, rol, entityName,loginDateTime,LogOutDateTime,docId,docName,machineId,reportId) 
+VALUES (18, 'Jorge', 2, 'Parque temático', '9:30 am', '6:00 pm', 18, 'Boleto de entrada', 20, 19)
+
+INSERT INTO Userlog (idUsuario,name, rol, entityName,loginDateTime,LogOutDateTime,docId,docName,machineId,reportId) 
+VALUES (19, 'Carla', 3, 'Centro comercial', '2:00 pm', '6:30 pm', 19, 'Recibo de compra', 8, 20)
+
+INSERT INTO Userlog (idUsuario,name, rol, entityName,loginDateTime,LogOutDateTime,docId,docName,machineId,reportId) 
+VALUES (20, ' Lapuerca',0 , ' teatro nacional', '3:54 pm' , '4:05 pm' , 20, ' Voucher de ventas ' ,32,21)
