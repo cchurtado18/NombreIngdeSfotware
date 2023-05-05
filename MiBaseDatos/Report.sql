0889d1cb969f6c8c189716bffcba84af0d3a049f
Edmund0-Sanchez <ejsanchez@uamv.edu.ni>
2023-05-04 23:56:11 -0600

    Reporte agregado

--- MiBaseDatos/Report.sql
+++ MiBaseDatos/Report.sql
@@ -1,2 +1,39 @@
 insert into Report (reportId,reportName,idUsuario,reportDateTime,identityName) Values (1,'Reporte 1','1','2023-05-04 14:30:00','NombreEntidad1')
+
 insert into Report (reportId,reportName,idUsuario,reportDateTime,identityName) Values (2,'Reporte 2','2','2023-03-02 09:15:00','NombreEntidad2')
+
+insert into Report (reportId,reportName,idUsuario,reportDateTime,identityName) Values (3,'Reporte 3','3','2023-05-06 11:45:00','NombreEntidad3')
+
+insert into Report (reportId,reportName,idUsuario,reportDateTime,identityName) Values (4,'Reporte 4','4','2023-05-07 15:20:00','NombreEntidad4')
+
+insert into Report (reportId,reportName,idUsuario,reportDateTime,identityName) Values (5,'Reporte 5','5','2023-05-08 10:10:00','NombreEntidad5')
+
+insert into Report (reportId,reportName,idUsuario,reportDateTime,identityName) Values (6,'Reporte 6','6','2023-05-09 13:00:00','NombreEntidad6')
+
+insert into Report (reportId,reportName,idUsuario,reportDateTime,identityName) Values (7,'Reporte 7','7','2023-05-10 08:30:00','NombreEntidad7')
+
+insert into Report (reportId,reportName,idUsuario,reportDateTime,identityName) Values (8,'Reporte 8','8','2023-05-11 16:45:00','NombreEntidad8')
+
+insert into Report (reportId,reportName,idUsuario,reportDateTime,identityName) Values (9,'Reporte 9','9','2023-05-12 12:00:00','NombreEntidad9')
+
+insert into Report (reportId,reportName,idUsuario,reportDateTime,identityName) Values (10,'Reporte 10','10','2023-05-13 14:20:00','NombreEntidad10')
+
+insert into Report (reportId,reportName,idUsuario,reportDateTime,identityName) Values (11,'Reporte 11','11','2023-05-14 09:00:00','NombreEntidad11')
+
+insert into Report (reportId,reportName,idUsuario,reportDateTime,identityName) Values (12,'Reporte 12','12','2023-05-15 11:30:00','NombreEntidad12')
+
+insert into Report (reportId,reportName,idUsuario,reportDateTime,identityName) Values (13,'Reporte 13','13','2023-05-16 15:15:00','NombreEntidad13')
+
+insert into Report (reportId,reportName,idUsuario,reportDateTime,identityName) Values (14,'Reporte 14','14','2023-05-17 10:45:00','NombreEntidad14')
+
+insert into Report (reportId,reportName,idUsuario,reportDateTime,identityName) Values (15,'Reporte 15','15','2023-05-18 13:20:00','NombreEntidad15')
+
+insert into Report (reportId,reportName,idUsuario,reportDateTime,identityName) Values (16,'Reporte 16','16','2023-05-19 08:00:00','NombreEntidad16')
+
+insert into Report (reportId,reportName,idUsuario,reportDateTime,identityName) Values (17,'Reporte 17','17','2023-05-20 11:10:00','NombreEntidad17')
+
+insert into Report (reportId,reportName,idUsuario,reportDateTime,identityName) Values (18,'Reporte 18','18','2023-05-21 14:50:00','NombreEntidad18')
+
+insert into Report (reportId,reportName,idUsuario,reportDateTime,identityName) Values (19,'Reporte 19','19','2023-05-22 09:30:00','NombreEntidad19')
+
+insert into Report (reportId,reportName,idUsuario,reportDateTime,identityName) Values (20,'Reporte 20','20','2023-05-23 08:30:00','NombreEntidad20')
