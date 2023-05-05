e9bfccdb9232f815196e441b7b7f29231e8f87e7
Gabriel Peralta <125324220+GP050620@users.noreply.github.com>
2023-05-05 00:57:13 -0600

    Commit docPermission

---
+++ MiBaseDatos/docPermission.sql
@@ -0,0 +1,70 @@
+INSERT INTO docPermission(userRole, leer, copiar, descargar, imprimir, dejarCompartir, comentar, reportePermit, idUsuario) 
+VALUES (0, 'false', 'false', 'false', 'false', 'false', 'false', SYSDATETIME(), 1);
+
+
+INSERT INTO docPermission(userRole, leer, copiar, descargar, imprimir, dejarCompartir, comentar, reportePermit, idUsuario) 
+VALUES (1, 'false', 'false', 'false', 'false', 'false', 'false', SYSDATETIME(), 2);
+
+
+INSERT INTO docPermission(userRole, leer, copiar, descargar, imprimir, dejarCompartir, comentar, reportePermit, idUsuario) 
+VALUES (2, 'false', 'false', 'false', 'false', 'false', 'false', SYSDATETIME(), 3);
+
+
+INSERT INTO docPermission(userRole, leer, copiar, descargar, imprimir, dejarCompartir, comentar, reportePermit, idUsuario) 
+VALUES (3, 'false', 'false', 'false', 'false', 'false', 'false', SYSDATETIME(), 4);
+
+INSERT INTO docPermission(userRole, leer, copiar, descargar, imprimir, dejarCompartir, comentar, reportePermit, idUsuario) 
+VALUES (4, 'false', 'false', 'false', 'false', 'false', 'false', SYSDATETIME(), 5);
+
+INSERT INTO docPermission(userRole, leer, copiar, descargar, imprimir, dejarCompartir, comentar, reportePermit, idUsuario) 
+VALUES (5, 'true', 'true', 'true', 'true', 'true', 'true', SYSDATETIME(), 6);
+
+INSERT INTO docPermission(userRole, leer, copiar, descargar, imprimir, dejarCompartir, comentar, reportePermit, idUsuario) 
+VALUES (0, 'false', 'false', 'false', 'false', 'false', 'false', SYSDATETIME(), 7);
+
+INSERT INTO docPermission(userRole, leer, copiar, descargar, imprimir, dejarCompartir, comentar, reportePermit, idUsuario) 
+VALUES (1, 'false', 'false', 'false', 'false', 'false', 'false', SYSDATETIME(), 8);
+
+INSERT INTO docPermission(userRole, leer, copiar, descargar, imprimir, dejarCompartir, comentar, reportePermit, idUsuario) 
+VALUES (2, 'false', 'false', 'false', 'false', 'false', 'false', SYSDATETIME(), 9);
+
+INSERT INTO docPermission(userRole, leer, copiar, descargar, imprimir, dejarCompartir, comentar, reportePermit, idUsuario) 
+VALUES (3, 'false', 'false', 'false', 'false', 'false', 'false', SYSDATETIME(), 10);
+
+INSERT INTO docPermission(userRole, leer, copiar, descargar, imprimir, dejarCompartir, comentar, reportePermit, idUsuario) 
+VALUES (4, 'false', 'false', 'false', 'false', 'false', 'false', SYSDATETIME(), 11);
+
+INSERT INTO docPermission(userRole, leer, copiar, descargar, imprimir, dejarCompartir, comentar, reportePermit, idUsuario) 
+VALUES (5, 'true', 'true', 'true', 'true', 'true', 'true', SYSDATETIME(), 12);
+
+
+INSERT INTO docPermission(userRole, leer, copiar, descargar, imprimir, dejarCompartir, comentar, reportePermit, idUsuario) 
+VALUES (0, 'false', 'false', 'false', 'false', 'false', 'false', SYSDATETIME(), 13);
+
+INSERT INTO docPermission(userRole, leer, copiar, descargar, imprimir, dejarCompartir, comentar, reportePermit, idUsuario) 
+VALUES (1, 'false', 'false', 'false', 'false', 'false', 'false', SYSDATETIME(), 14);
+
+
+INSERT INTO docPermission(userRole, leer, copiar, descargar, imprimir, dejarCompartir, comentar, reportePermit, idUsuario) 
+VALUES (2, 'false', 'false', 'false', 'false', 'false', 'false', SYSDATETIME(), 15);
+
+
+INSERT INTO docPermission(userRole, leer, copiar, descargar, imprimir, dejarCompartir, comentar, reportePermit, idUsuario) 
+VALUES (3, 'false', 'false', 'false', 'false', 'false', 'false', SYSDATETIME(), 16);
+
+INSERT INTO docPermission(userRole, leer, copiar, descargar, imprimir, dejarCompartir, comentar, reportePermit, idUsuario) 
+VALUES (4, 'false', 'false', 'false', 'false', 'false', 'false', SYSDATETIME(), 17);
+
+
+INSERT INTO docPermission(userRole, leer, copiar, descargar, imprimir, dejarCompartir, comentar, reportePermit, idUsuario) 
+VALUES (5, 'true', 'true', 'true', 'true', 'true', 'true', SYSDATETIME(), 18);
+
+
+INSERT INTO docPermission(userRole, leer, copiar, descargar, imprimir, dejarCompartir, comentar, reportePermit, idUsuario) 
+VALUES (0, 'false', 'false', 'false', 'false', 'false', 'false', SYSDATETIME(), 19);
+
+INSERT INTO docPermission(userRole, leer, copiar, descargar, imprimir, dejarCompartir, comentar, reportePermit, idUsuario) 
+VALUES (1, 'false', 'false', 'false', 'false', 'false', 'false', SYSDATETIME(), 20);
+
+
+select * from docPermission
