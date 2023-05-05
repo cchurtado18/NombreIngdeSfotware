055e2b99e83b3f588ef6b6c2a5e22567974e3b0c
Gabriel Peralta <125324220+GP050620@users.noreply.github.com>
2023-05-05 01:13:40 -0600

    userPermission Commit

---
+++ MiBaseDatos/userPermission.sql
@@ -0,0 +1,62 @@
+insert into userPermission(userRole,idUsuario,createUser,deleteUser,modifyUser)
+values(0,1,'false','false','false')
+
+insert into userPermission(userRole,idUsuario,createUser,deleteUser,modifyUser)
+values(1,2,'false','false','false')
+
+insert into userPermission(userRole,idUsuario,createUser,deleteUser,modifyUser)
+values(2,3,'false','false','false')
+
+insert into userPermission(userRole,idUsuario,createUser,deleteUser,modifyUser)
+values(3,4,'false','false','false')
+
+insert into userPermission(userRole,idUsuario,createUser,deleteUser,modifyUser)
+values(4,5,'false','false','false')
+
+
+insert into userPermission(userRole,idUsuario,createUser,deleteUser,modifyUser)
+values(5,6,'true','true','true')
+
+
+insert into userPermission(userRole,idUsuario,createUser,deleteUser,modifyUser)
+values(0,7,'false','false','false')
+
+insert into userPermission(userRole,idUsuario,createUser,deleteUser,modifyUser)
+values(1,8,'false','false','false')
+
+insert into userPermission(userRole,idUsuario,createUser,deleteUser,modifyUser)
+values(2,9,'false','false','false')
+
+
+insert into userPermission(userRole,idUsuario,createUser,deleteUser,modifyUser)
+values(3,10,'false','false','false')
+
+insert into userPermission(userRole,idUsuario,createUser,deleteUser,modifyUser)
+values(4,11,'false','false','false')
+
+insert into userPermission(userRole,idUsuario,createUser,deleteUser,modifyUser)
+values(5,12,'true','true','true')
+
+insert into userPermission(userRole,idUsuario,createUser,deleteUser,modifyUser)
+values(0,13,'false','false','false')
+
+insert into userPermission(userRole,idUsuario,createUser,deleteUser,modifyUser)
+values(1,14,'false','false','false')
+
+insert into userPermission(userRole,idUsuario,createUser,deleteUser,modifyUser)
+values(2,15,'false','false','false')
+
+insert into userPermission(userRole,idUsuario,createUser,deleteUser,modifyUser)
+values(3,16,'false','false','false')
+
+insert into userPermission(userRole,idUsuario,createUser,deleteUser,modifyUser)
+values(4,17,'false','false','false')
+
+insert into userPermission(userRole,idUsuario,createUser,deleteUser,modifyUser)
+values(5,18,'true','true','true')
+
+insert into userPermission(userRole,idUsuario,createUser,deleteUser,modifyUser)
+values(0,19,'false','false','false')
+
+insert into userPermission(userRole,idUsuario,createUser,deleteUser,modifyUser)
+values(1,20,'false','false','false')
