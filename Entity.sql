3fafabd94c73d1caa5dbc10b00d93a8f494aaa2b
Gabriel Peralta <125324220+GP050620@users.noreply.github.com>
2023-05-11 21:23:41 -0600

    Commit tabla Entidad

---
+++ Entity.sql
@@ -0,0 +1,57 @@
+INSERT INTO entidad (NombreEntidad,UsuarioCreacion,FechaCreacion,UsuarioEdicion,FechaEdicion) VALUES ('NombreEntidad1','Usuario1','01-01-2000','Usuario1','01-01-2000');
+
+INSERT INTO entidad (NombreEntidad,UsuarioCreacion,FechaCreacion,UsuarioEdicion,FechaEdicion) VALUES ('NombreEntidad2','Usuario2','01-02-2000','Usuario2','01-02-2000');
+
+
+INSERT INTO entidad (NombreEntidad,UsuarioCreacion,FechaCreacion,UsuarioEdicion,FechaEdicion) VALUES ('NombreEntidad3','Usuario3','01-03-2000','Usuario3','01-03-2000');
+
+
+INSERT INTO entidad (NombreEntidad,UsuarioCreacion,FechaCreacion,UsuarioEdicion,FechaEdicion) VALUES ('NombreEntidad4','Usuario4','01-04-2000','Usuario4','01-04-2000');
+
+
+INSERT INTO entidad (NombreEntidad,UsuarioCreacion,FechaCreacion,UsuarioEdicion,FechaEdicion) VALUES ('NombreEntidad5','Usuario5','01-05-2000','Usuario5','01-05-2000');
+
+
+INSERT INTO entidad (NombreEntidad,UsuarioCreacion,FechaCreacion,UsuarioEdicion,FechaEdicion) VALUES ('NombreEntidad6','Usuario6','01-06-2000','Usuario6','01-06-2000');
+
+
+INSERT INTO entidad (NombreEntidad,UsuarioCreacion,FechaCreacion,UsuarioEdicion,FechaEdicion) VALUES ('NombreEntidad7','Usuario7','01-07-2000','Usuario7','01-07-2000');
+
+
+INSERT INTO entidad (NombreEntidad,UsuarioCreacion,FechaCreacion,UsuarioEdicion,FechaEdicion) VALUES ('NombreEntidad8','Usuario8','01-08-2000','Usuario8','01-08-2000');
+
+
+INSERT INTO entidad (NombreEntidad,UsuarioCreacion,FechaCreacion,UsuarioEdicion,FechaEdicion) VALUES ('NombreEntidad9','Usuario9','01-09-2000','Usuario9','01-09-2000');
+
+
+INSERT INTO entidad (NombreEntidad,UsuarioCreacion,FechaCreacion,UsuarioEdicion,FechaEdicion) VALUES ('NombreEntidad10','Usuario10','01-10-2000','Usuario10','01-10-2000');
+
+
+INSERT INTO entidad (NombreEntidad,UsuarioCreacion,FechaCreacion,UsuarioEdicion,FechaEdicion) VALUES ('NombreEntidad11','Usuario11','01-11-2000','Usuario11','01-11-2000');
+
+
+INSERT INTO entidad (NombreEntidad,UsuarioCreacion,FechaCreacion,UsuarioEdicion,FechaEdicion) VALUES ('NombreEntidad12','Usuario12','01-12-2000','Usuario12','01-12-2000');
+
+
+INSERT INTO entidad (NombreEntidad,UsuarioCreacion,FechaCreacion,UsuarioEdicion,FechaEdicion) VALUES ('NombreEntidad13','Usuario13','01-13-2000','Usuario13','01-13-2000');
+
+
+INSERT INTO entidad (NombreEntidad,UsuarioCreacion,FechaCreacion,UsuarioEdicion,FechaEdicion) VALUES ('NombreEntidad14','Usuario14','01-14-2000','Usuario14','01-14-2000');
+
+
+INSERT INTO entidad (NombreEntidad,UsuarioCreacion,FechaCreacion,UsuarioEdicion,FechaEdicion) VALUES ('NombreEntidad15','Usuario15','01-15-2000','Usuario15','01-15-2000');
+
+
+INSERT INTO entidad (NombreEntidad,UsuarioCreacion,FechaCreacion,UsuarioEdicion,FechaEdicion) VALUES ('NombreEntidad16','Usuario16','01-16-2000','Usuario16','01-16-2000');
+
+
+INSERT INTO entidad (NombreEntidad,UsuarioCreacion,FechaCreacion,UsuarioEdicion,FechaEdicion) VALUES ('NombreEntidad17','Usuario17','01-17-2000','Usuario17','01-17-2000');
+
+
+INSERT INTO entidad (NombreEntidad,UsuarioCreacion,FechaCreacion,UsuarioEdicion,FechaEdicion) VALUES ('NombreEntidad18','Usuario18','01-18-2000','Usuario18','01-18-2000');
+
+
+INSERT INTO entidad (NombreEntidad,UsuarioCreacion,FechaCreacion,UsuarioEdicion,FechaEdicion) VALUES ('NombreEntidad19','Usuario19','01-19-2000','Usuario19','01-19-2000');
+
+
+INSERT INTO entidad (NombreEntidad,UsuarioCreacion,FechaCreacion,UsuarioEdicion,FechaEdicion) VALUES ('NombreEntidad20','Usuario20','01-20-2000','Usuario20','01-20-2000');
