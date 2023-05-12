55aa2cb86723884e4f578d14fbec820d58602c3e
RobinOP20 <crobregon@uamv.edu.ni>
2023-05-11 19:43:32 -0600

    RolAgregado

---
+++ Rol.sql
@@ -0,0 +1,39 @@
+INSERT INTO perfil.Rol (IdRol, IdUsuario, NombreRol, Estado) VALUES (1, 1, 'Administrador', 1);
+
+INSERT INTO perfil.Rol (IdRol, IdUsuario, NombreRol, Estado) VALUES (2, 2, 'Editor', 1);
+
+INSERT INTO perfil.Rol (IdRol, IdUsuario, NombreRol, Estado) VALUES (3, 3, 'Lector', 1);
+
+INSERT INTO perfil.Rol (IdRol, IdUsuario, NombreRol, Estado) VALUES (4, 4, 'Invitado', 0);
+
+INSERT INTO perfil.Rol (IdRol, IdUsuario, NombreRol, Estado) VALUES (5, 5, 'Supervisor', 1);
+
+INSERT INTO perfil.Rol (IdRol, IdUsuario, NombreRol, Estado) VALUES (6, 6, 'Analista', 0);
+
+INSERT INTO perfil.Rol (IdRol, IdUsuario, NombreRol, Estado) VALUES (7, 7, 'Desarrollador', 1);
+
+INSERT INTO perfil.Rol (IdRol, IdUsuario, NombreRol, Estado) VALUES (8, 8, 'Tester', 0);
+
+INSERT INTO perfil.Rol (IdRol, IdUsuario, NombreRol, Estado) VALUES (9, 9, 'Diseñador', 1);
+
+INSERT INTO perfil.Rol (IdRol, IdUsuario, NombreRol, Estado) VALUES (10, 10, 'Consultor', 1);
+
+INSERT INTO perfil.Rol (IdRol, IdUsuario, NombreRol, Estado) VALUES (11, 11, 'Gerente', 1);
+
+INSERT INTO perfil.Rol (IdRol, IdUsuario, NombreRol, Estado) VALUES (12, 12, 'DBA', 1);
+
+INSERT INTO perfil.Rol (IdRol, IdUsuario, NombreRol, Estado) VALUES (13, 13, 'Scrum Master', 0);
+
+INSERT INTO perfil.Rol (IdRol, IdUsuario, NombreRol, Estado) VALUES (14, 14, 'Product Owner', 1);
+
+INSERT INTO perfil.Rol (IdRol, IdUsuario, NombreRol, Estado) VALUES (15, 15, 'Arquitecto', 0);
+
+INSERT INTO perfil.Rol (IdRol, IdUsuario, NombreRol, Estado) VALUES (16, 16, 'CEO', 1);
+
+INSERT INTO perfil.Rol (IdRol, IdUsuario, NombreRol, Estado) VALUES (17, 17, 'CTO', 1);
+
+INSERT INTO perfil.Rol (IdRol, IdUsuario, NombreRol, Estado) VALUES (18, 18, 'CFO', 0);
+
+INSERT INTO perfil.Rol (IdRol, IdUsuario, NombreRol, Estado) VALUES (19, 19, 'CIO', 1);
+
+INSERT INTO perfil.Rol (IdRol, IdUsuario, NombreRol, Estado) VALUES (20, 20, 'Auditor', 1);
