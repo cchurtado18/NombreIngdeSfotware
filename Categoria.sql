42a8d716207deb29451cb2c45d97b20933eb918b
RobinOP20 <crobregon@uamv.edu.ni>
2023-05-11 19:48:22 -0600

    CategoriaAgregado

---
+++ Categoria.sql
@@ -0,0 +1,39 @@
+INSERT INTO archivo.Categoria (IdCategoria, NombreCategoria) VALUES (1, 'Jardín');
+
+INSERT INTO archivo.Categoria (IdCategoria, NombreCategoria) VALUES (2, 'Viajes');
+
+INSERT INTO archivo.Categoria (IdCategoria, NombreCategoria) VALUES (3, 'Arte');
+
+INSERT INTO archivo.Categoria (IdCategoria, NombreCategoria) VALUES (4, 'Instrumentos musicales');
+
+INSERT INTO archivo.Categoria (IdCategoria, NombreCategoria) VALUES (5, 'Salud');
+
+INSERT INTO archivo.Categoria (IdCategoria, NombreCategoria) VALUES (6, 'Fotografía');
+
+INSERT INTO archivo.Categoria (IdCategoria, NombreCategoria) VALUES (7, 'Herramientas');
+
+INSERT INTO archivo.Categoria (IdCategoria, NombreCategoria) VALUES (8, 'Computación');
+
+INSERT INTO archivo.Categoria (IdCategoria, NombreCategoria) VALUES (9, 'Muebles');
+
+INSERT INTO archivo.Categoria (IdCategoria, NombreCategoria) VALUES (10, 'Oficina');
+
+INSERT INTO archivo.Categoria (IdCategoria, NombreCategoria) VALUES (11, 'Electrónica');
+
+INSERT INTO archivo.Categoria (IdCategoria, NombreCategoria) VALUES (12, 'Hogar');
+
+INSERT INTO archivo.Categoria (IdCategoria, NombreCategoria) VALUES (13, 'Deportes');
+
+INSERT INTO archivo.Categoria (IdCategoria, NombreCategoria) VALUES (14, 'Moda');
+
+INSERT INTO archivo.Categoria (IdCategoria, NombreCategoria) VALUES (15, 'Alimentación');
+
+INSERT INTO archivo.Categoria (IdCategoria, NombreCategoria) VALUES (16, 'Juguetes');
+
+INSERT INTO archivo.Categoria (IdCategoria, NombreCategoria) VALUES (17, 'Belleza');
+
+INSERT INTO archivo.Categoria (IdCategoria, NombreCategoria) VALUES (18, 'Mascotas');
+
+INSERT INTO archivo.Categoria (IdCategoria, NombreCategoria) VALUES (19, 'Libros');
+
+INSERT INTO archivo.Categoria (IdCategoria, NombreCategoria) VALUES (20, 'Automóvil');
