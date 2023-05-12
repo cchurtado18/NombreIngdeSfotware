46d0cc427d8620ba0e1bb3c202366f295d9d2931
Edmund0-Sanchez <ejsanchez@uamv.edu.ni>
2023-05-11 18:37:35 -0600

    Reporte agregado

---
+++ MiBaseDatos/Reporte.sql
@@ -0,0 +1,39 @@
+insert into perfil.Reporte (IdReporte, IdDocumento, IdDocHistorial, NombreReporte, FechaReporte) Values (1, 1 , 1 ,'Reporte1','2023-05-04 14:30:00')
+
+insert into perfil.Reporte (IdReporte, IdDocumento, IdDocHistorial, NombreReporte, FechaReporte) values (2, 2, 2, 'Reporte2', '2023-05-05 15:45:00')
+
+insert into perfil.Reporte (IdReporte, IdDocumento, IdDocHistorial, NombreReporte, FechaReporte) values (3, 3, 3, 'Reporte3', '2023-05-06 09:20:00')
+
+insert into perfil.Reporte (IdReporte, IdDocumento, IdDocHistorial, NombreReporte, FechaReporte) values (4, 4, 4, 'Reporte4', '2023-05-07 16:10:00')
+
+insert into perfil.Reporte (IdReporte, IdDocumento, IdDocHistorial, NombreReporte, FechaReporte) values (5, 5, 5, 'Reporte5', '2023-05-08 11:35:00')
+
+insert into perfil.Reporte (IdReporte, IdDocumento, IdDocHistorial, NombreReporte, FechaReporte) values (6, 6, 6, 'Reporte6', '2023-05-09 14:55:00')
+
+insert into perfil.Reporte (IdReporte, IdDocumento, IdDocHistorial, NombreReporte, FechaReporte) values (7, 7, 7, 'Reporte7', '2023-05-10 13:00:00')
+
+insert into perfil.Reporte (IdReporte, IdDocumento, IdDocHistorial, NombreReporte, FechaReporte) values (8, 8, 8, 'Reporte8', '2023-05-11 08:45:00')
+
+insert into perfil.Reporte (IdReporte, IdDocumento, IdDocHistorial, NombreReporte, FechaReporte) values (9, 9, 9, 'Reporte9', '2023-05-12 12:20:00')
+
+insert into perfil.Reporte (IdReporte, IdDocumento, IdDocHistorial, NombreReporte, FechaReporte) values (10, 10, 10, 'Reporte10', '2023-05-13 17:30:00')
+
+insert into perfil.Reporte (IdReporte, IdDocumento, IdDocHistorial, NombreReporte, FechaReporte) values (11, 11, 11, 'Reporte11', '2023-05-14 10:05:00')
+
+insert into perfil.Reporte (IdReporte, IdDocumento, IdDocHistorial, NombreReporte, FechaReporte) values (12, 12, 12, 'Reporte12', '2023-05-15 14:15:00')
+
+insert into perfil.Reporte (IdReporte, IdDocumento, IdDocHistorial, NombreReporte, FechaReporte) values (13, 13, 13, 'Reporte13', '2023-05-16 11:30:00')
+
+insert into perfil.Reporte (IdReporte, IdDocumento, IdDocHistorial, NombreReporte, FechaReporte) values (14, 14, 14, 'Reporte14', '2023-05-17 16:45:00')
+
+insert into perfil.Reporte (IdReporte, IdDocumento, IdDocHistorial, NombreReporte, FechaReporte) values (15, 15, 15, 'Reporte15', '2023-05-18 09:10:00')
+
+insert into perfil.Reporte (IdReporte, IdDocumento, IdDocHistorial, NombreReporte, FechaReporte) values (16, 16, 16, 'Reporte16', '2023-05-19 14:30:00')
+
+insert into perfil.Reporte (IdReporte, IdDocumento, IdDocHistorial, NombreReporte, FechaReporte) values (17, 17, 17, 'Reporte17', '2023-05-20 11:55:00')
+
+insert into perfil.Reporte (IdReporte, IdDocumento, IdDocHistorial, NombreReporte, FechaReporte) values (18, 18, 18, 'Reporte18', '2023-05-21 15:10:00')
+
+insert into perfil.Reporte (IdReporte, IdDocumento, IdDocHistorial, NombreReporte, FechaReporte) values (19, 19, 19, 'Reporte19', '2023-05-22 12:25:00')
+
+insert into perfil.Reporte (IdReporte, IdDocumento, IdDocHistorial, NombreReporte, FechaReporte) values (20, 20, 20, 'Reporte20', '2023-05-23 17:50:00')
