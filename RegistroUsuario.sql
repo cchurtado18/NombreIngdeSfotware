2b0276549642c30f2eac89ac370ee82e150ea521
Carlos H <117938637+cchurtado18@users.noreply.github.com>
2023-05-11 20:24:15 -0600

    Add files via upload

---
+++ RegistroUsuario.sql
@@ -0,0 +1,61 @@
+insert into perfil.RegistroUsuario(	IdRegistro, IdUsuario ,IdDocumento ,IdReporte,	NombreRegistro,  	FechaIngreso ,	FechaSalida,	UsuarioCreacion,  FechaCreacion, UsuarioEdicion, FechaEdicion )
+values ( 2 , 1 , 12 , 3 ,'Reporte de cchurtado', '13/03/2023 10:15:30' ,' 13/03/2023 11:25:30 ','13/03/2023 10:15:30', 'Admin', '13/03/2023 10:15:30');
+
+
+INSERT INTO perfil.RegistroUsuario (IdRegistro, IdUsuario, IdDocumento, IdReporte, NombreRegistro, FechaIngreso, FechaSalida, UsuarioCreacion, FechaCreacion, UsuarioEdicion, FechaEdicion)
+VALUES (2, 1, 12, 3, 'Reporte de cchurtado', '13/03/2023 10:15:30', '13/03/2023 11:25:30', '13/03/2023 10:15:30', 'Admin', '13/03/2023 10:15:30');
+
+INSERT INTO perfil.RegistroUsuario (IdRegistro, IdUsuario, IdDocumento, IdReporte, NombreRegistro, FechaIngreso, FechaSalida, UsuarioCreacion, FechaCreacion, UsuarioEdicion, FechaEdicion)
+VALUES (3, 2, 10, 5, 'Reporte de usuario2', '14/03/2023 09:30:45', '14/03/2023 10:45:20', '14/03/2023 09:30:45', 'Admin', '14/03/2023 09:30:45');
+
+INSERT INTO perfil.RegistroUsuario (IdRegistro, IdUsuario, IdDocumento, IdReporte, NombreRegistro, FechaIngreso, FechaSalida, UsuarioCreacion, FechaCreacion, UsuarioEdicion, FechaEdicion)
+VALUES (4, 3, 8, 1, 'Reporte de usuario3', '15/03/2023 14:20:10', '15/03/2023 15:30:25', '15/03/2023 14:20:10', 'Admin', '15/03/2023 14:20:10');
+
+INSERT INTO perfil.RegistroUsuario (IdRegistro, IdUsuario, IdDocumento, IdReporte, NombreRegistro, FechaIngreso, FechaSalida, UsuarioCreacion, FechaCreacion, UsuarioEdicion, FechaEdicion)
+VALUES (5, 4, 6, 2, 'Reporte de usuario4', '16/03/2023 11:45:55', '16/03/2023 12:50:30', '16/03/2023 11:45:55', 'Admin', '16/03/2023 11:45:55');
+
+INSERT INTO perfil.RegistroUsuario (IdRegistro, IdUsuario, IdDocumento, IdReporte, NombreRegistro, FechaIngreso, FechaSalida, UsuarioCreacion, FechaCreacion, UsuarioEdicion, FechaEdicion)
+VALUES (6, 5, 4, 4, 'Reporte de usuario5', '17/03/2023 08:15:40', '17/03/2023 09:25:10', '17/03/2023 08:15:40', 'Admin', '17/03/2023 08:15:40');
+
+INSERT INTO perfil.RegistroUsuario (IdRegistro, IdUsuario, IdDocumento, IdReporte, NombreRegistro, FechaIngreso, FechaSalida, UsuarioCreacion, FechaCreacion, UsuarioEdicion, FechaEdicion)
+VALUES (7, 6, 2, 6, 'Reporte de usuario6', '18/03/2023 13:10:20', '18/03/2023 14:32:14' , '17/03/2023 08:15:40', 'Admin', '17/03/2023 08:15:40');
+
+INSERT INTO perfil.RegistroUsuario (IdRegistro, IdUsuario, IdDocumento, IdReporte, NombreRegistro, FechaIngreso, FechaSalida, UsuarioCreacion, FechaCreacion, UsuarioEdicion, FechaEdicion)
+VALUES (8, 7, 14, 7, 'Reporte de usuario7', '19/03/2023 16:30:50', '19/03/2023 17:45:15', '19/03/2023 16:30:50', 'Admin', '19/03/2023 16:30:50');
+
+INSERT INTO perfil.RegistroUsuario (IdRegistro, IdUsuario, IdDocumento, IdReporte, NombreRegistro, FechaIngreso, FechaSalida, UsuarioCreacion, FechaCreacion, UsuarioEdicion, FechaEdicion)
+VALUES (9, 8, 11, 8, 'Reporte de usuario8', '20/03/2023 09:55:10', '20/03/2023 11:05:30', '20/03/2023 09:55:10', 'Admin', '20/03/2023 09:55:10');
+
+INSERT INTO perfil.RegistroUsuario (IdRegistro, IdUsuario, IdDocumento, IdReporte, NombreRegistro, FechaIngreso, FechaSalida, UsuarioCreacion, FechaCreacion, UsuarioEdicion, FechaEdicion)
+VALUES (10, 9, 13, 9, 'Reporte de usuario9', '21/03/2023 14:40:25', '21/03/2023 15:50:45', '21/03/2023 14:40:25', 'Admin', '21/03/2023 14:40:25');
+
+INSERT INTO perfil.RegistroUsuario (IdRegistro, IdUsuario, IdDocumento, IdReporte, NombreRegistro, FechaIngreso, FechaSalida, UsuarioCreacion, FechaCreacion, UsuarioEdicion, FechaEdicion)
+VALUES (11, 10, 9, 10, 'Reporte de usuario10', '22/03/2023 12:25:55', '22/03/2023 13:35:10', '22/03/2023 12:25:55', 'Admin', '22/03/2023 12:25:55');
+
+INSERT INTO perfil.RegistroUsuario (IdRegistro, IdUsuario, IdDocumento, IdReporte, NombreRegistro, FechaIngreso, FechaSalida, UsuarioCreacion, FechaCreacion, UsuarioEdicion, FechaEdicion)
+VALUES (12, 11, 7, 11, 'Reporte de usuario11', '23/03/2023 07:50:40', '23/03/2023 09:00:20', '23/03/2023 07:50:40', 'Admin', '23/03/2023 07:50:40');
+
+INSERT INTO perfil.RegistroUsuario (IdRegistro, IdUsuario, IdDocumento, IdReporte, NombreRegistro, FechaIngreso, FechaSalida, UsuarioCreacion, FechaCreacion, UsuarioEdicion, FechaEdicion)
+VALUES (20, 19, 8, 19, 'Reporte de usuario19', '31/03/2023 14:15:35', '31/03/2023 15:25:50', '31/03/2023 14:15:35', 'Admin', '31/03/2023 14:15:35');
+
+INSERT INTO perfil.RegistroUsuario (IdRegistro, IdUsuario, IdDocumento, IdReporte, NombreRegistro, FechaIngreso, FechaSalida, UsuarioCreacion, FechaCreacion, UsuarioEdicion, FechaEdicion)
+VALUES (14, 13, 3, 13, 'Reporte de usuario13', '25/03/2023 10:45:30', '25/03/2023 11:55:45', '25/03/2023 10:45:30', 'Admin', '25/03/2023 10:45:30');
+
+INSERT INTO perfil.RegistroUsuario (IdRegistro, IdUsuario, IdDocumento, IdReporte, NombreRegistro, FechaIngreso, FechaSalida, UsuarioCreacion, FechaCreacion, UsuarioEdicion, FechaEdicion)
+VALUES (15, 14, 1, 14, 'Reporte de usuario14', '26/03/2023 08:20:55', '26/03/2023 09:30:10', '26/03/2023 08:20:55', 'Admin', '26/03/2023 08:20:55');
+
+INSERT INTO perfil.RegistroUsuario (IdRegistro, IdUsuario, IdDocumento, IdReporte, NombreRegistro, FechaIngreso, FechaSalida, UsuarioCreacion, FechaCreacion, UsuarioEdicion, FechaEdicion)
+VALUES (16, 15, 6, 15, 'Reporte de usuario15', '27/03/2023 13:55:40', '27/03/2023 15:05:20', '27/03/2023 13:55:40', 'Admin', '27/03/2023 13:55:40');
+
+INSERT INTO perfil.RegistroUsuario (IdRegistro, IdUsuario, IdDocumento, IdReporte, NombreRegistro, FechaIngreso, FechaSalida, UsuarioCreacion, FechaCreacion, UsuarioEdicion, FechaEdicion)
+VALUES (17, 16, 9, 16, 'Reporte de usuario16', '28/03/2023 11:30:10', '28/03/2023 12:40:30', '28/03/2023 11:30:10', 'Admin', '28/03/2023 11:30:10');
+
+INSERT INTO perfil.RegistroUsuario (IdRegistro, IdUsuario, IdDocumento, IdReporte, NombreRegistro, FechaIngreso, FechaSalida, UsuarioCreacion, FechaCreacion, UsuarioEdicion, FechaEdicion)
+VALUES (18, 17, 12, 17, 'Reporte de usuario17', '29/03/2023 16:05:50', '29/03/2023 17:15:15', '29/03/2023 16:05:50', 'Admin', '29/03/2023 16:05:50');
+
+INSERT INTO perfil.RegistroUsuario (IdRegistro, IdUsuario, IdDocumento, IdReporte, NombreRegistro, FechaIngreso, FechaSalida, UsuarioCreacion, FechaCreacion, UsuarioEdicion, FechaEdicion)
+VALUES (19, 18, 15, 18, 'Reporte de usuario18', '30/03/2023 09:40:25', '30/03/2023 17:15:15', '29/03/2023 16:05:50', 'Admin', '29/03/2023 16:05:50');
+
+INSERT INTO perfil.RegistroUsuario (IdRegistro, IdUsuario, IdDocumento, IdReporte, NombreRegistro, FechaIngreso, FechaSalida, UsuarioCreacion, FechaCreacion, UsuarioEdicion, FechaEdicion)
+VALUES (16, 15, 6, 15, 'Reporte de usuario15', '27/03/2023 13:55:40', '27/03/2023 15:05:20', '27/03/2023 13:55:40', 'Admin', '27/03/2023 13:55:40');
+
