cac90c56528185c1cc98ee9effdc6c91daccf287
Edmund0-Sanchez <117939996+Edmund0-Sanchez@users.noreply.github.com>
2023-05-25 23:17:40 -0600

    Reporte agregado

---
+++ Reporte.sql
@@ -0,0 +1,39 @@
+INSERT INTO perfil.Reporte (idDocumento, IdDocHistorial, NombreReporte) VALUES (1, 1, 'Reporte1');
+
+INSERT INTO perfil.Reporte (idDocumento, IdDocHistorial, NombreReporte) VALUES (2, 2, 'Reporte2');
+
+INSERT INTO perfil.Reporte (idDocumento, IdDocHistorial, NombreReporte) VALUES (3, 3, 'Reporte3');
+
+INSERT INTO perfil.Reporte (idDocumento, IdDocHistorial, NombreReporte) VALUES (4, 4, 'Reporte4');
+
+INSERT INTO perfil.Reporte (idDocumento, IdDocHistorial, NombreReporte) VALUES (5, 5, 'Reporte5');
+
+INSERT INTO perfil.Reporte (idDocumento, IdDocHistorial, NombreReporte) VALUES (6, 6, 'Reporte6');
+
+INSERT INTO perfil.Reporte (idDocumento, IdDocHistorial, NombreReporte) VALUES (7, 7, 'Reporte7');
+
+INSERT INTO perfil.Reporte (idDocumento, IdDocHistorial, NombreReporte) VALUES (8, 8, 'Reporte8');
+
+INSERT INTO perfil.Reporte (idDocumento, IdDocHistorial, NombreReporte) VALUES (9, 9, 'Reporte9');
+
+INSERT INTO perfil.Reporte (idDocumento, IdDocHistorial, NombreReporte) VALUES (10, 10, 'Reporte10');
+
+INSERT INTO perfil.Reporte (idDocumento, IdDocHistorial, NombreReporte) VALUES (11, 11, 'Reporte11');
+
+INSERT INTO perfil.Reporte (idDocumento, IdDocHistorial, NombreReporte) VALUES (12, 12, 'Reporte12');
+
+INSERT INTO perfil.Reporte (idDocumento, IdDocHistorial, NombreReporte) VALUES (13, 13, 'Reporte13');
+
+INSERT INTO perfil.Reporte (idDocumento, IdDocHistorial, NombreReporte) VALUES (14, 14, 'Reporte14');
+
+INSERT INTO perfil.Reporte (idDocumento, IdDocHistorial, NombreReporte) VALUES (15, 15, 'Reporte15');
+
+INSERT INTO perfil.Reporte (idDocumento, IdDocHistorial, NombreReporte) VALUES (16, 16, 'Reporte16');
+
+INSERT INTO perfil.Reporte (idDocumento, IdDocHistorial, NombreReporte) VALUES (17, 17, 'Reporte17');
+
+INSERT INTO perfil.Reporte (idDocumento, IdDocHistorial, NombreReporte) VALUES (18, 18, 'Reporte18');
+
+INSERT INTO perfil.Reporte (idDocumento, IdDocHistorial, NombreReporte) VALUES (19, 19, 'Reporte19');
+
+INSERT INTO perfil.Reporte (idDocumento, IdDocHistorial, NombreReporte) VALUES (20, 20, 'Reporte20');
