730c99282c57fd8277b6bbd5a691a01acdb3e43c
RobinOP20 <113996771+RobinOP20@users.noreply.github.com>
2023-05-26 16:00:43 -0600

    Rol agregado

---
+++ Rol.sql
@@ -0,0 +1,4 @@
+INSERT INTO perfil.rol (NombreRol, Estado) VALUES ('Administrador', 1)
+INSERT INTO perfil.rol (NombreRol, Estado) VALUES ('Basico', 1)
+INSERT INTO perfil.rol (NombreRol, Estado) VALUES ('Intermedio', 1)
+INSERT INTO perfil.rol (NombreRol, Estado) VALUES ('Avanzado', 1)
