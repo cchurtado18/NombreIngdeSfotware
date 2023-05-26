41c1e6fb23b363b60e8ad2ffa354b0260ebcd4e9
Carlos H <117938637+cchurtado18@users.noreply.github.com>
2023-05-26 16:05:34 -0600

    Add files via upload

---
+++ RegistroUsuario.sql
@@ -0,0 +1,37 @@
+INSERT INTO perfil.RegistroUsuario (IdUsuario, IdDocumento, IdReporte, NombreRegistro) VALUES (2, 2, 2, 'Registro 2');
+
+INSERT INTO perfil.RegistroUsuario (IdUsuario, IdDocumento, IdReporte, NombreRegistro) VALUES (3, 3, 3, 'Registro 3');
+
+INSERT INTO perfil.RegistroUsuario (IdUsuario, IdDocumento, IdReporte, NombreRegistro) VALUES (4, 4, 4, 'Registro 4');
+
+INSERT INTO perfil.RegistroUsuario (IdUsuario, IdDocumento, IdReporte, NombreRegistro) VALUES (5, 5, 5, 'Registro 5');
+
+INSERT INTO perfil.RegistroUsuario (IdUsuario, IdDocumento, IdReporte, NombreRegistro) VALUES (6, 6, 6, 'Registro 6');
+
+INSERT INTO perfil.RegistroUsuario (IdUsuario, IdDocumento, IdReporte, NombreRegistro) VALUES (7, 7, 7, 'Registro 7');
+
+INSERT INTO perfil.RegistroUsuario (IdUsuario, IdDocumento, IdReporte, NombreRegistro) VALUES (8, 8, 8, 'Registro 8');
+
+INSERT INTO perfil.RegistroUsuario (IdUsuario, IdDocumento, IdReporte, NombreRegistro) VALUES (9, 9, 9, 'Registro 9');
+
+INSERT INTO perfil.RegistroUsuario (IdUsuario, IdDocumento, IdReporte, NombreRegistro) VALUES (10, 10, 10, 'Registro 10');
+
+INSERT INTO perfil.RegistroUsuario (IdUsuario, IdDocumento, IdReporte, NombreRegistro) VALUES (11, 11, 11, 'Registro 11');
+
+INSERT INTO perfil.RegistroUsuario (IdUsuario, IdDocumento, IdReporte, NombreRegistro) VALUES (12, 12, 12, 'Registro 12');
+
+INSERT INTO perfil.RegistroUsuario (IdUsuario, IdDocumento, IdReporte, NombreRegistro) VALUES (13, 13, 13, 'Registro 13');
+
+INSERT INTO perfil.RegistroUsuario (IdUsuario, IdDocumento, IdReporte, NombreRegistro) VALUES (14, 14, 14, 'Registro 14');
+
+INSERT INTO perfil.RegistroUsuario (IdUsuario, IdDocumento, IdReporte, NombreRegistro) VALUES (15, 15, 15, 'Registro 15');
+
+INSERT INTO perfil.RegistroUsuario (IdUsuario, IdDocumento, IdReporte, NombreRegistro) VALUES (16, 16, 16, 'Registro 16');
+
+INSERT INTO perfil.RegistroUsuario (IdUsuario, IdDocumento, IdReporte, NombreRegistro) VALUES (17, 17, 17, 'Registro 17');
+
+INSERT INTO perfil.RegistroUsuario (IdUsuario, IdDocumento, IdReporte, NombreRegistro) VALUES (18, 18, 18, 'Registro 18');
+
+INSERT INTO perfil.RegistroUsuario (IdUsuario, IdDocumento, IdReporte, NombreRegistro) VALUES (19, 19, 19, 'Registro 19');
+
+INSERT INTO perfil.RegistroUsuario (IdUsuario, IdDocumento, IdReporte, NombreRegistro) VALUES (20, 20, 20, 'Registro 20');
